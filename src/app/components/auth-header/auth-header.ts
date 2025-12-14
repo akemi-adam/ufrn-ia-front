ec53157b75c6881077fca16bb2877f534aca04fd
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-12-14 12:34:51 -0300

    new: componente de header das telas de auth

---
+++ src/app/components/auth-header/auth-header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UfrnLogo } from '../../atoms/ufrn-logo/ufrn-logo';
+
+@Component({
+  selector: 'app-auth-header',
+  imports: [UfrnLogo],
+  templateUrl: './auth-header.html',
+  styleUrl: './auth-header.css',
+})
+export class AuthHeader {
+
+}
