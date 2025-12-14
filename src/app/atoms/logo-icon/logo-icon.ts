611f16fddfd63641a2c6ccd9fd161761d58502b2
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-12-14 12:29:15 -0300

    new: componente de logo base

---
+++ src/app/atoms/logo-icon/logo-icon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-icon',
+  imports: [],
+  templateUrl: './logo-icon.html',
+  styleUrl: './logo-icon.css',
+})
+export class LogoIcon {
+
+}
