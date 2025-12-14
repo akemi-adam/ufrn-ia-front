41bbf4285b80a240559785c9192362f5cbe8a215
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-12-14 12:29:00 -0300

    new: componente de input base

---
+++ src/app/atoms/input/input.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.css',
+})
+export class Input {
+  placeholder = input<string>('')
+  type = input<string>('')
+  name = input<string>('')
+}
